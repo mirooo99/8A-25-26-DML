a0dfb8b5f627159e8ce16e9d1c90a48fd39c6233
Ddd1612 <ddd.hristov16@gmail.com>
2026-05-14 11:43:30 +0300

    Initial commit

---
+++ Denis/auth.js
@@ -0,0 +1,5 @@
+const { createClient } = supabase;
+const db = createClient(
+  'https://usdjvroyetttrahiniwu.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzZGp2cm95ZXR0dHJhaGluaXd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1ODQxNTksImV4cCI6MjA5NDE2MDE1OX0.SFwT8rOF8r_euCo6AEGtmaYS8Dfts160jFBQJ2_ay9I'
+);
